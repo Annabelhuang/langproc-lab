16cb953e5af4d49cb2a4befb23038e3da6207b24
AnnabelHuang <ah2423@ic.ac.uk>
2025-02-10 11:56:12 +0000

    lab3

--- 3-codegen/src/ast_compile.cpp
+++ 3-codegen/src/ast_compile.cpp
@@ -21,20 +21,72 @@ void CompileRec(
         std::cout<<"const "<<destReg<<" "<<program->type<<std::endl;
         
     }else if( regex_match( program->type, reId ) ){
-        std::string zero = makeName("zero");
-        std::cout<<"const "<<zero<<" 0"<<std::endl;
-        std::cout<<"add "<<destReg<<" "<<program->type<<" "<<zero<<std::endl;
+        std::cout<<"add "<<destReg<<" "<<program->type<<" "<<" zero "<<std::endl;
 
     }else if(program->type=="Param"){
         std::cout<<"param "<<destReg<<" "<<program->value<<std::endl;
 
-    }else if(program->type=="Seq"){
+    }else if(program->type=="Input"){
+        std::cout<<"input "<<destReg<<std::endl;
+
+    }else if(program->type=="Output"){
+        CompileRec(destReg, program->branches[0]);
+        std::cout<<"output "<<destReg<<std::endl;
+
+    }else if(program->type=="LessThan"){
+        std::string srcRegA = makeName("srcRegA");
+        CompileRec(srcRegA, program->branches[0]);
+        std::string srcRegB = makeName("srcRegB");
+        CompileRec(srcRegB, program->branches[1]);
+        std::cout<<"lt "<<destReg<<" "<<srcRegA<<" "<<srcRegB<<std::endl;
+
+    }else if(program->type=="Add"){
+        std::string srcRegA = makeName("asrcRegA");
+        CompileRec(srcRegA, program->branches[0]);
+        std::string srcRegB = makeName("srcRegB");
+        CompileRec(srcRegB, program->branches[1]);
+        std::cout<<"add "<<destReg<<" "<<srcRegA<<" "<<srcRegB<<std::endl;
+
+    }else if(program->type=="Sub"){
+        std::string srcRegA = makeName("srcRegA");
+        CompileRec(srcRegA, program->branches[0]);
+        std::string srcRegB = makeName("srcRegB");
+        CompileRec(srcRegB, program->branches[1]);
+        std::cout<<"sub "<<destReg<<" "<<srcRegA<<" "<<srcRegB<<std::endl;
+
+    }else if(program->type=="Assign"){
+        std::string v_reg = makeName("v");
+        CompileRec(v_reg, program->branches[0]);
+        std::cout<<"add "<<program->value<<" "<<v_reg<<" "<<" zero "<<std::endl;
+        std::cout<<"add "<<destReg<<" "<<v_reg<<" "<<" zero "<<std::endl;
+
+    }else if(program->type=="If"){
+        std::string srcRegA = makeName("srcRegA");
+        CompileRec(srcRegA, program->branches[0]);
+        std::string else_label = makeName("else");
+        std::string end_label = makeName("endif");
+        std::cout<<"beq "<<srcRegA<<" zero "<<else_label<<std::endl;
+        CompileRec(destReg, program->branches[1]);
+        std::cout<<"beq zero zero "<<end_label<<std::endl;
+        std::cout<<":"<<else_label<<std::endl;
+        CompileRec(destReg, program->branches[2]);
+        std::cout<<":"<<end_label<<std::endl;
+    }else if (program->type == "While") {
+        std::string loop_start = makeName("loop_start");
+        std::string loop_end = makeName("loop_end");
+        std::string srcRegA = makeName("srcRegA");
+        std::cout << ":" << loop_start << std::endl;
+        CompileRec(srcRegA, program->branches[0]);
+        std::cout << "beq " << srcRegA << " zero " << loop_end << std::endl;
+        CompileRec(destReg, program->branches[1]);
+        std::cout << "beq zero zero " << loop_start << std::endl;
+        std::cout << ":" << loop_end << std::endl;
+        std::cout << "const " << destReg << " 0" << std::endl;
+    }
+    else if(program->type=="Seq"){
         for(unsigned i=0; i<program->branches.size(); i++){
             CompileRec(destReg, program->branches[i]);
         }
-    
-    // TODO : handle the others
-        
     }else{
         throw std::runtime_error("Unknown construct '"+program->type+"'");
     }
@@ -43,6 +95,7 @@ void CompileRec(
 void Compile(
     TreePtr program
 ){
+    std::cout << "const zero 0" << std::endl;
     // Create a register to hold the final result
     std::string res=makeName("res");
     
--- 3-codegen/src/ast_interpret.cpp
+++ 3-codegen/src/ast_interpret.cpp
@@ -8,17 +8,25 @@ int32_t Interpret(
 ){
     std::regex reNum("^-?[0-9]+$");
     std::regex reId("^[a-z][a-z0-9]*$");
-    
-    if( regex_match(program->type, reNum) ){
+    if (regex_match(program->type, reNum)) {
         return std::atol(program->type.c_str());
-        
+    } 
     // TODO : Check for things matching reId
-        
+    else if (regex_match(program->type, reId)) {
+        // Retrieve variable value
+        if (context.bindings.find(program->type) == context.bindings.end()) {
+            throw std::runtime_error("Undefined variable: " + program->type);
+        }
+        return context.bindings[program->type];
     }else if(program->type=="Param"){
         unsigned index=atol(program->value.c_str());
         auto value=context.params.at(index);
         return value;
         
+    }else if (program->type == "Input") {
+        int value;
+        std::cin >> value;  
+        return value;       
     }else if(program->type=="Output"){
         int32_t val=Interpret(context, program->branches.at(0));
         std::cout<<val<<std::endl;
@@ -26,6 +34,36 @@ int32_t Interpret(
         
     // TODO: Handle other constructs
         
+    } else if (program->type == "Add") {
+        return Interpret(context, program->branches.at(0)) + 
+               Interpret(context, program->branches.at(1));
+    }else if (program->type == "Sub") {
+        return Interpret(context, program->branches.at(0)) - 
+               Interpret(context, program->branches.at(1));
+    }else if (program->type == "LessThan") {
+        return Interpret(context, program->branches.at(0)) < 
+               Interpret(context, program->branches.at(1));
+    }else if (program->type == "Assign") {
+        int32_t val = Interpret(context, program->branches.at(0));
+        context.bindings[program->value] = val;
+        return val;
+    }else if (program->type == "Seq") {
+        int32_t result = 0;
+        for (auto &stmt : program->branches) {
+            result = Interpret(context, stmt);
+        }
+        return result;
+    }else if (program->type == "If") {
+        if (Interpret(context, program->branches.at(0))) {
+            return Interpret(context, program->branches.at(1));
+        } else {
+            return Interpret(context, program->branches.at(2));
+        }
+    }else if (program->type == "While") {
+        while (Interpret(context, program->branches.at(0))) {
+            Interpret(context, program->branches.at(1));
+        }
+        return 0;
     }else{
         throw std::runtime_error("Unknown construct '"+program->type+"'");
     }
